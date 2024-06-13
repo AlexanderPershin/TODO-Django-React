1dd78e7be34abe470f7418daa9da26e66847b323
Alexander Pershin <alexanderpershinwebdeveloper@gmail.com>
2024-06-13 21:43:52 +0700

    simplified layout and basic navigation added

---
+++ frontend/src/Components/Layout/Simplified.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import { Flex, Layout } from "antd";
+import { HomeOutlined } from "@ant-design/icons";
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const headerStyle: React.CSSProperties = {
+    textAlign: "center",
+    color: "#000",
+    height: 64,
+    paddingInline: 48,
+    lineHeight: "64px",
+    backgroundColor: "transparent",
+};
+
+const contentStyle: React.CSSProperties = {
+    textAlign: "center",
+    minHeight: "100%",
+    lineHeight: "120px",
+    color: "#000",
+    backgroundColor: "transparent",
+};
+
+const footerStyle: React.CSSProperties = {
+    textAlign: "center",
+    color: "#000",
+    backgroundColor: "transparent",
+};
+
+const layoutStyle = {
+    overflow: "auto",
+    width: "100%",
+    maxWidth: "100vw",
+    minHeight: "100vh",
+    padding: "1rem",
+};
+
+const Simplified = () => {
+    return (
+        <Layout style={layoutStyle}>
+            <Header style={headerStyle}>
+                <Link to="/">
+                    <HomeOutlined />
+                </Link>{" "}
+                Stack: Django React Docker
+            </Header>
+            <Content style={contentStyle}>
+                <Outlet />
+            </Content>
+            <Footer style={footerStyle}>
+                &copy; All rights reserved, Alexander Pershin,{" "}
+                {new Date().getFullYear()}
+            </Footer>
+        </Layout>
+    );
+};
+
+export default Simplified;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+    createHashRouter,
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import "./style.css";
+
+import App from "./App.tsx";
+import ErrorPage from "./ErrorPage.tsx";
+import Login from "./Pages/Login.tsx";
+import Simplified from "./Components/Layout/Simplified.tsx";
+import Default from "./Components/Layout/Default.tsx";
+import Todos from "./Pages/Todos.tsx";
+import Welcome from "./Pages/Welcome.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Simplified />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <App />,
+                errorElement: <ErrorPage />,
+            },
+            {
+                path: "/login",
+                element: <Login />,
+                errorElement: <ErrorPage />,
+            },
+        ],
+    },
+    {
+        path: "/todos",
+        element: <Default />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/todos",
+                element: <Welcome />,
+                errorElement: <ErrorPage />,
+            },
+            {
+                path: "/todos/all",
+                element: <Todos />,
+                errorElement: <ErrorPage />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
